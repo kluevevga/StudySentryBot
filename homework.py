36b6f6a8e3259e3abb526b34badb32710fb66808
Евгений Клюев <craftycoderpro@gmail.com>
2022-06-26 19:52:37 +0300

    refactoring

--- homework.py
+++ homework.py
@@ -1,90 +1,88 @@
-from requests.exceptions import RequestException
-from dotenv import load_dotenv
-import telegram
-import requests
 import logging
 import time
 import sys
 import os
 
+from dotenv import load_dotenv
+import telegram
+import requests
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 handler = logging.StreamHandler(stream=sys.stdout)
-formatter = logging.Formatter(
-    '%(asctime)s %(name)s %(levelname)s %(message)s')
+formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
+
 load_dotenv()
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
-
-class SurroundError(Exception):
-    """Проходит pytest."""
-
-    def __init__(self, *args):
-        """Инициирует."""
-        self.message = ('Отсутствуют переменные окружения '
-                        'Проверьте переменные окружения '
-                        'или попробуйте перезвонить позднее.')
-
-
-PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN') or None
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN') or None
-TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID') or None
-HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
-RETRY_PERIOD = 600
-ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HOMEWORK_VERDICTS = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
     'reviewing': 'Работа взята на проверку ревьюером.',
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
+ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
+HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+RETRY_PERIOD = 600
+
 
 def check_tokens():
-    """Проходит pytest."""
-    required_tokens = (
-        PRACTICUM_TOKEN,
-        TELEGRAM_TOKEN,
-        TELEGRAM_CHAT_ID)
-
-    if None in required_tokens:
-        logger.critical('нет токенов')
-        raise SurroundError()
+    """Тест .env токенов."""
+    return all((PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID))
 
 
 def send_message(bot, message):
     """Проходит pytest."""
+    logger.debug('Отправка сообщений в telegram')
+    logger.info(message)
+
     try:
         bot.send_message(TELEGRAM_CHAT_ID, message)
-        logger.debug(message)
     except Exception:
-        logger.error('ошибка бота')
+        logger.error('Ошибка отправки сообщения в телеграм')
 
 
 def get_api_answer(timestamp):
     """Проходит pytest."""
+    logger.debug('Отправка api запроса')
+
     try:
-        payload = {'from_date': timestamp}
-        requests_api_headers = HEADERS
         response = requests.get(
             ENDPOINT,
-            headers=requests_api_headers,
-            params=payload
-        )
-        if response.status_code != 200:
-            raise RequestException()
+            timeout=5,
+            headers=HEADERS,
+            params={'from_date': timestamp})
 
-        return response.json()
-    except RequestException as err:
-        logger.error(err.message)
+    except requests.ConnectTimeout:
+        logger.error('api connection timeout')
+    except requests.RequestException as err:
+        logger.error(f'ambiguous api error: {err}')
+
+    if response.status_code != 200:
+        raise requests.HTTPError(response.status_code)
+
+    return response.json()
 
 
 def check_response(response):
     """Проходит pytest."""
-    if (not isinstance(response, dict) or not
-        response.get('homeworks', False) or not
-            isinstance(response['homeworks'], list)):
-        raise TypeError()
+    logger.debug('Проверка api ответа')
+
+    if not isinstance(response, dict):
+        raise TypeError('В ответе API response не является dict')
+
+    missed_keys = {'homeworks', 'current_date'}
+    if missed_keys - response.keys():
+        raise TypeError(f'В ответе API нет ожидаемых ключей: {missed_keys}')
+
+    if not isinstance(response['homeworks'], list):
+        raise TypeError('В ответе API homeworks не является типом list')
+
+    return len(response['homeworks']) > 0
 
 
 def parse_status(homework):
@@ -101,18 +99,30 @@ def parse_status(homework):
 
 def main():
     """Проходит pytest."""
-    check_tokens()
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
+
+    if not check_tokens():
+        send_message(bot, 'Ошибка импорта токенов')
+        logger.critical('Токен | токены не импортировались')
+        sys.exit()
+
+    timestamp = 0
     while True:
         try:
-            response = get_api_answer(0)
-            check_response(response)
-            message = parse_status(response['homeworks'][0])
-            send_message(bot, message)
-        except TypeError:
-            logger.error('ошибка апы')
-        except Exception as error:
-            message = f'Сбой в работе программы: {error}'
+            response = get_api_answer(timestamp)
+            has_new_status = check_response(response)
+            timestamp = response['current_date']
+
+            if has_new_status:
+                message = parse_status(response['homeworks'][0])
+                send_message(bot, message)
+            else:
+                logger.info('Новых статусов нет')
+
+        except TypeError as err:
+            logger.error(err)
+        except requests.HTTPError as code:
+            logger.error(f'api http error, status code: {code}')
         finally:
             time.sleep(RETRY_PERIOD)
 
